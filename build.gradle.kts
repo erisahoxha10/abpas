fc17d0a83e84d42ccc2be378167df5210543f168
erisa <erisa.hoxha@ffuf.de>
2022-07-03 01:11:32 +0200

    add manifest

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,3 +37,9 @@ tasks.withType<KotlinCompile> {
 tasks.withType<Test> {
 	useJUnitPlatform()
 }
+
+tasks.withType<Jar> {
+	manifest {
+		attributes["Main-Class"] = "com.abpas.AbpasApplication"
+	}
+}
