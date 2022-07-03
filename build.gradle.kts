2ed1966b4adfe4fd96d1e7a22d55b00880c405cf
erisa <erisa.hoxha@ffuf.de>
2022-07-03 02:23:05 +0200

    fix gradle - another stupid change

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 	id("org.jetbrains.kotlin.jvm") version "1.5.10"
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
 	id("org.springframework.boot") version "2.7.1"
+	application
 //	id("io.spring.dependency-management") version "1.0.11.RELEASE"
 //	kotlin("jvm") version "1.6.21"
 //	kotlin("plugin.spring") version "1.6.21"
@@ -43,9 +44,13 @@ dependencies {
 tasks.withType<Test> {
 	useJUnitPlatform()
 }
+//
+//tasks.withType<Jar> {
+//	manifest {
+//		attributes["Main-Class"] = "src.main.kotlin.com.abpas.AbpasApplication"
+//	}
+//}
 
-tasks.withType<Jar> {
-	manifest {
-		attributes["Main-Class"] = "com.abpas.AbpasApplication"
-	}
+application() {
+	mainClass.set("com.abpas.AbpasApplication")
 }
